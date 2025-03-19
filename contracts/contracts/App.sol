222abe35f438d370f15447b529c2acc5d1ef8ca8
Tonsak <tonsak.gs@gmail.com>
2025-03-19 18:35:32 +0700

    refactor App contract to enhance event and ticket management; add event creation, ticket addition, and ownership validation functions

--- contracts/contracts/App.sol
+++ contracts/contracts/App.sol
@@ -5,9 +5,16 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 struct Ticket {
+    uint id;
     uint total;
     uint[] tokens;
-    bool resalable;
+    uint event_id;
+}
+
+struct Event {
+    uint id;
+    address owner;
+    uint resale_rate;
 }
 
 struct Token {
@@ -20,49 +27,84 @@ struct Token {
 contract App is ERC721, Ownable(msg.sender) {
     uint private _token_id;
 
+    mapping(uint => Event) public events;
     mapping(uint => Token) public tokens;
     mapping(uint => Ticket) public tickets;
+    mapping(address => Token[]) public owned;
 
     constructor() ERC721("APP", "AP") {}
 
-    function createTicket(
-        uint _id,
-        uint _total,
-        bool resalable
-    ) external onlyOwner {
-        tickets[_id] = Ticket({
-            total: _total,
-            resalable: resalable,
+    function create_event(uint id, uint resale_rate) external onlyOwner {
+        events[id] = Event({
+            id: id,
+            owner: msg.sender,
+            resale_rate: resale_rate
+        });
+    }
+
+    function add_ticket(uint event_id, uint id, uint total) external onlyOwner {
+        require(events[event_id].owner != address(0), "Event does not exist");
+
+        Event storage ev = events[event_id];
+
+        require(ev.owner == msg.sender, "You are not the event owner");
+
+        tickets[id] = Ticket({
+            id: id,
+            total: total,
+            event_id: event_id,
             tokens: new uint[](0)
         });
     }
 
-    function buyTicket(
-        uint ticket_id,
-        uint amount,
-        address buyer
-    ) external onlyOwner {
+    function buy_ticket(uint ticket_id, uint amount) external onlyOwner {
         Ticket storage ticket = tickets[ticket_id];
 
-        require(ticket.total > 0, "Ticket does not exist");
         require(
-            ticket.tokens.length + amount <= ticket.total,
-            "Not enough tickets available"
+            ticket.total - ticket.tokens.length >= amount,
+            "Not enough tickets"
         );
 
         for (uint i = 0; i < amount; i++) {
-            _mint(buyer, _token_id);
-
-            ticket.tokens.push(_token_id);
+            _token_id++;
 
             tokens[_token_id] = Token({
                 id: _token_id,
                 used: false,
-                owner: buyer,
-                ticket_id: ticket_id
+                owner: msg.sender,
+                ticket_id: ticket.id
             });
 
-            _token_id++;
+            ticket.tokens.push(_token_id);
+            owned[msg.sender].push(tokens[_token_id]);
         }
+    }
+
+    function use_ticket(uint event_id, uint token_id) external onlyOwner {
+        Event storage ev = events[event_id];
+        Token storage token = tokens[token_id];
+
+        require(ev.owner == msg.sender, "You are not the event owner");
+        require(!token.used, "Ticket already used");
+
+        token.used = true;
+    }
+
+    function resale_ticket(uint token_id, address to) external onlyOwner {
+        Token storage token = tokens[token_id];
+
+        require(token.owner == msg.sender, "You are not the ticket owner");
+
+        token.owner = to;
+    }
+
+    function get_owned_tickets() external view returns (Token[] memory) {
+        return owned[msg.sender];
+    }
+
+    function get_token_info(
+        uint token_id
+    ) external view returns (Token memory) {
+        return tokens[token_id];
     }
 }
