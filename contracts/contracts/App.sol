afd48b29b844c1c646fd6d52316e8a183208046a
Tonsak <tonsak.gs@gmail.com>
2025-03-19 17:11:42 +0700

    refactor App contract to enhance ticket management; replace ERC721URIStorage with ERC721, streamline ticket creation, and update token handling

--- contracts/contracts/App.sol
+++ contracts/contracts/App.sol
@@ -2,47 +2,39 @@
 pragma solidity ^0.8.26;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 struct Ticket {
-    uint sold;
     uint total;
     uint[] tokens;
+    bool resalable;
 }
 
-contract App is ERC721URIStorage, Ownable(msg.sender) {
+struct Token {
+    uint id;
+    bool used;
+    address owner;
+    uint ticket_id;
+}
+
+contract App is ERC721, Ownable(msg.sender) {
     uint private _token_id;
-    uint private _ticket_id;
 
-    mapping(uint => bool) public used;
+    mapping(uint => Token) public tokens;
     mapping(uint => Ticket) public tickets;
-    mapping(address => uint[]) public owned;
 
-    constructor() payable ERC721("APP", "AP") {}
-
-    event CreateTicket(uint indexed ticket_id, uint total, string metadata);
+    constructor() ERC721("APP", "AP") {}
 
     function createTicket(
+        uint _id,
         uint _total,
-        string memory metadata
+        bool resalable
     ) external onlyOwner {
-        tickets[_ticket_id] = Ticket({
-            sold: 0,
+        tickets[_id] = Ticket({
             total: _total,
-            tokens: new uint[](_total)
+            resalable: resalable,
+            tokens: new uint[](0)
         });
-
-        for (uint i = 0; i < _total; i++) {
-            _mint(address(this), _token_id);
-            _setTokenURI(_token_id, metadata);
-
-            used[_token_id] = false;
-            tickets[_ticket_id].tokens[i] = _token_id++;
-        }
-
-        emit CreateTicket(_ticket_id, _total, metadata);
-
-        _ticket_id++;
     }
 
     function buyTicket(
@@ -54,44 +46,23 @@ contract App is ERC721URIStorage, Ownable(msg.sender) {
 
         require(ticket.total > 0, "Ticket does not exist");
         require(
-            ticket.sold + amount <= ticket.total,
+            ticket.tokens.length + amount <= ticket.total,
             "Not enough tickets available"
         );
 
         for (uint i = 0; i < amount; i++) {
-            _transfer(address(this), buyer, ticket.tokens[ticket.sold++]);
+            _mint(buyer, _token_id);
 
-            owned[buyer].push(_token_id);
+            ticket.tokens.push(_token_id);
+
+            tokens[_token_id] = Token({
+                id: _token_id,
+                used: false,
+                owner: buyer,
+                ticket_id: ticket_id
+            });
+
+            _token_id++;
         }
-    }
-
-    function transferTicket(
-        address from,
-        address to,
-        uint _id
-    ) external onlyOwner {
-        require(used[_id] == false, "Ticket already used");
-
-        _transfer(from, to, _id);
-
-        for (uint i = 0; i < owned[from].length; i++) {
-            if (owned[from][i] == _id) {
-                owned[from][i] = owned[from][owned[from].length - 1];
-                owned[from].pop();
-
-                break;
-            }
-        }
-    }
-
-    function verifyTicket(uint ticket_id, uint _id) external onlyOwner {
-        require(tickets[ticket_id].total > 0, "Ticket does not exist");
-        require(used[_id] == false, "Ticket already used");
-
-        used[_id] = true;
-    }
-
-    function renounceOwnership() public view override onlyOwner {
-        revert("Can't renounce ownership here");
     }
 }
--- contracts/ignition/modules/App.ts
+++ contracts/ignition/modules/App.ts
@@ -1,9 +1,7 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 export default buildModule("App", (m) => {
-  const c = m.contract("App", [], {
-    value: 1000000000000000000n,
-  });
+  const c = m.contract("App", []);
 
   return { c };
 });
