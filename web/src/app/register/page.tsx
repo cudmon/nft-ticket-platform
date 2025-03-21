30833d87c7f2c1d60989cc289277bbcfc9880de8
W. Romrampha <katang346@gmail.com>
2025-03-22 03:30:34 +0700

    Update register page UI

--- web/src/app/register/page.tsx
+++ web/src/app/register/page.tsx
@@ -1,5 +1,72 @@
+"use client";
+import { useState } from 'react';
+import { TextInput, PasswordInput, Container, Title, Group, Button, Fieldset, Text } from '@mantine/core';
+import { Mail, Lock, User } from 'lucide-react';
+
+interface Form {
+    name: string;
+    email: string;
+    password: string;
+}
+
 export default function page() {
+    const [formData, setFormData] = useState<Form>({
+        name: '',
+        email: '',
+        password: '',
+    });
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFormData({ ...formData, [e.currentTarget.name]: e.currentTarget.value });
+    };
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log(formData);
+    };
+
     return (
-        <h1>Hello world</h1>
+        <Container size={600} my={60}>
+            <Fieldset>
+                <Title order={1} ta='center' mt='md' mb='md'>Register</Title>
+                <form onSubmit={handleSubmit}>
+                    <TextInput 
+                        name='name'
+                        label='Name' 
+                        placeholder='Name'
+                        value={formData.name}
+                        onChange={handleInputChange}
+                        leftSection={<User/>}
+                        withAsterisk
+                    />
+                    <TextInput 
+                        name='email'
+                        label='Email' 
+                        placeholder='user@example.com'
+                        value={formData.email}
+                        onChange={handleInputChange}
+                        leftSection={<Mail/>}
+                        withAsterisk
+                        mt='md'
+                    />
+                    <PasswordInput 
+                        name='password'
+                        label='Password' 
+                        placeholder='Your password'
+                        value={formData.password}
+                        onChange={handleInputChange}
+                        leftSection={<Lock/>}
+                        withAsterisk
+                        mt='md'
+                    />
+                    <Group align='center' mt='lg'>
+                        <Button type='submit' variant='filled' fullWidth>Register</Button>
+                    </Group>
+                </form>
+                <Text ta='center' mt='md' size='sm' c='blue'>
+                    Already have an account? <a href='/login'> Sign in →</a>
+                </Text>
+            </Fieldset>
+        </Container>
     )
 }
