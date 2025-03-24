689449d4b4020aaeda0f147a8780f35dd8ee9df8
Mok <mokmaard646@gmail.com>
2025-03-24 12:54:12 +0700

    feat(event): create event component

--- web/src/app/events/[eventId]/page.tsx
+++
@@ -1,8 +0,0 @@
-import { useRouter } from "next/router"
-
-export default function page() {
-    const router = useRouter();
-    return (
-        <h1>{router.query.eventId}</h1>
-    )
-} 
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,9 +1,26 @@
-import { Input } from '@mantine/core';
+'use client';
+
+import EventCard from '@/components/event-card';
+import { Flex, Grid, Image } from '@mantine/core';
 
 export default function Page() {
   return (
-    <p>Hello workd
-      
-    </p>
+      <>
+
+        <Flex direction={"column"}
+              gap={"md"}>
+          <Image radius={"md"}
+                  h={"100"}
+                  src={"https://placehold.co/100x20"}/>
+          <Grid>
+            <Grid.Col span={4}>
+              <EventCard eventName='Post Malone'
+                         eventDescription='ทดสอบ'
+                         eventId='15'
+                         eventStatus='Available'/>
+            </Grid.Col>
+          </Grid>
+        </Flex>
+      </>
   );
 }
