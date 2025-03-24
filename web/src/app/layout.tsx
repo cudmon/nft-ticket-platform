c6fc5853f19dd5cc818f415a5d9cb0e8e3483e12
Mok <mokmaard646@gmail.com>
2025-03-24 12:52:54 +0700

    refactor(layout): import navbar.

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import '@mantine/core/styles.css';
 import { ColorSchemeScript, MantineProvider, mantineHtmlProps } from '@mantine/core';
-import { Button, Input, Flex, Box } from '@mantine/core';
-import { ShoppingCart, Search } from 'lucide-react';
+import { Box } from '@mantine/core';
+import Navbar from '@/components/navbar';
 
 export default function RootLayout({ children,
 }: {
@@ -16,30 +16,9 @@ export default function RootLayout({ children,
       <body>
         <MantineProvider>
 
-          <Box px='md'>
-
-            <Flex justify='space-between'
-                  align={'center'}>
-              <h1>TicketHub</h1>
-              
-              <Flex justify='space-between'
-                    align={'center'}
-                    gap={'md'}
-                    pos={'sticky'}>
-
-                <Input placeholder="Search events.." 
-                      leftSection={<Search/>}
-                      size='md'/>
-                <Button color='black'
-                        size='md'>
-                          <ShoppingCart />
-                </Button>
-                <Button size='md'>Sign In</Button>
-
-              </Flex>
-
-            </Flex>
-
+          <Box pos={'relative'}>
+            <Navbar/>
+            
             {children}
           </Box>
         </MantineProvider>
