5be6b38de25a62af8ce45811a25e0981a4945762
Mok <mokmaard646@gmail.com>
2025-03-24 14:43:10 +0700

    feat(frontend, Footer): create and initial footer.

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -2,6 +2,7 @@ import '@mantine/core/styles.css';
 import { ColorSchemeScript, MantineProvider, mantineHtmlProps } from '@mantine/core';
 import { Box } from '@mantine/core';
 import Navbar from '@/components/navbar';
+import Footer from '@/components/footer';
 
 export default function RootLayout({ children,
 }: {
@@ -13,13 +14,17 @@ export default function RootLayout({ children,
         <ColorSchemeScript />
       </head>
 
-      <body>
+      <body style={
+        {
+          overflow: 'scroll',
+        }
+      }>
         <MantineProvider>
 
           <Box pos={'relative'}>
             <Navbar/>
-            
             {children}
+            <Footer/>
           </Box>
         </MantineProvider>
       </body>
